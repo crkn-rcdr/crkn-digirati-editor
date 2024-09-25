60f19af3e2e86ff522d58995084994f5065e65f2
Brittny Lapierre <brittny.lapierre@gmail.com>
2024-09-25 14:43:37 -0400

    Adding prod server

---
+++ server.js
@@ -0,0 +1,28 @@
+import Fastify from 'fastify'
+import fastifyStatic from '@fastify/static'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.register(fastifyStatic, {
+    root: path.join(__dirname, 'dist'),
+})
+
+fastify.get('/', function (req, reply) {
+  reply.sendFile('index.html')
+})
+
+// Run the server!
+fastify.listen({ port: 3000 , host: '0.0.0.0' }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    fastify.log.info(`server listening on ${address}`)
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,65 @@
-import { ManifestEditor } from "manifest-editor";
-import "manifest-editor/dist/index.css";
+import { ManifestEditor } from "manifest-editor"
+import "manifest-editor/dist/index.css"
 import './App.css'
-
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react" //useCallback
+import { useExistingVault } from "react-iiif-vault"
+import { useSaveVault } from "@manifest-editor/shell"
 
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState()
+  const manifestId = "https://www.canadiana.ca/iiif/oocihm.8_06911_32/manifest" 
+  const vault = useExistingVault()
  
+  const saveVault = useCallback(() => {
+    // Save logic here.
+    console.log("Reading from vault")
+    const vaultData = vault.getObject(manifestId)
+    if(typeof vaultData !== "undefined") {
+      setData(vaultData)
+      // Save the data to localstorage
+      const dataString = JSON.stringify(vaultData)
+      console.log("ds: ", dataString)
+      localStorage.setItem(manifestId, dataString)
+      //const dataString2 = JSON.stringify(vault.getState().iiif.entities.Manifest)
+      //console.log("ds2: ", dataString2)
+    } else {
+      console.log("Vault data undef")
+    }
+
+    
+  }, [])
+ 
+  useSaveVault(
+    // The instance of the vault.
+    vault,
+    // Callback to save the vault.
+    saveVault,
+    // How often it should save the vault (debounce)
+    5000
+  )
+
   useEffect(() => {
-    fetch("https://www.canadiana.ca/iiif/oocihm.8_06911_32/manifest")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-      });
-  }, []);
+    const vaultDataText = localStorage.getItem(manifestId)
+    if(typeof vaultDataText === "string" && vaultDataText !== "null") {
+      console.log("Localstore:", vaultDataText )
+      vault.loadManifestSync(manifestId, JSON.parse(vaultDataText))
+    } else {
+      fetch(manifestId)
+        .then((res) => res.json())
+        .then((data) => {
+          vault.loadManifestSync(manifestId, data)
+        })
+    }
+  }, [])
  
   if (!data) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
 
   return (
     <>
       <div style={{ width: "100vw", height: "100vh", display: "flex" }}>
-        <ManifestEditor resource={{ id: data["id"], type: "Manifest" }} data={data} />
+        <ManifestEditor resource={{ id: manifestId, type: "Manifest" }} data={data}/>
       </div>
     </>
   )
