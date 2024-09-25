183a17bb91c410fba243bd7616b9c223fdb14582
Brittny Lapierre <brittny.lapierre@gmail.com>
2024-09-25 13:10:43 -0400

    Attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import { ManifestEditor } from "manifest-editor";
 import "manifest-editor/dist/index.css";
 import './App.css'
-
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"; //useCallback
+// import { useExistingVault } from "react-iiif-vault";
+// import { useSaveVault } from "@manifest-editor/shell";
 
 function App() {
   const [data, setData] = useState();
+  const manifestId = "https://www.canadiana.ca/iiif/oocihm.8_06911_32/manifest" 
+  /*const vault = useExistingVault()
  
+  const saveVault = useCallback(() => {
+    // Save logic here.
+    console.log("Eh?")
+    console.log(vault.getObject(manifestId))
+  }, []);
+ 
+  useSaveVault(
+    // The instance of the vault.
+    vault,
+    // Callback to save the vault.
+    saveVault,
+    // How often it should save the vault (debounce)
+    5000
+  );*/
+
   useEffect(() => {
-    fetch("https://www.canadiana.ca/iiif/oocihm.8_06911_32/manifest")
+    fetch(manifestId)
       .then((res) => res.json())
       .then((data) => {
-        setData(data);
+        //vault.loadManifestSync(manifestId, data)
+        setData(data)
       });
   }, []);
  
@@ -22,7 +41,7 @@ function App() {
   return (
     <>
       <div style={{ width: "100vw", height: "100vh", display: "flex" }}>
-        <ManifestEditor resource={{ id: data["id"], type: "Manifest" }} data={data} />
+        <ManifestEditor resource={{ id: manifestId, type: "Manifest" }} data={data}/>
       </div>
     </>
   )
